d4e60fb3f09c4128a5b9e1fe019e9fef02163d6a
Dharnish D P <dharnishdp@Dharnishs-MacBook-Pro.local>
2025-01-09 09:54:15 +0530

    updated the code

--- filter.py
+++ filter.py
@@ -4,9 +4,11 @@ import csv
 
 root_log_folder = os.path.join(os.getcwd(), 'csvs')
 
+month = {'Jan':2,'Feb':0,'Mar':0,'Apr':2,'May':1,'Jun':0,'Jul':0,'Aug':1,'Sep':0,'Oct':2,'Nov':1,'Dec':1}
+
 if os.path.exists(root_log_folder):
-    log_files = [f for f in os.listdir(root_log_folder) if os.path.isfile(os.path.join(root_log_folder, f))]
-    log_files = [f for f in log_files if f.endswith('.csv')]
+    log_files_file = [f for f in os.listdir(root_log_folder) if os.path.isfile(os.path.join(root_log_folder, f))]
+    log_files = [f for f in log_files_file if f.endswith('.csv')]
 
 data = {}
 for file in log_files:
@@ -15,9 +17,9 @@ for file in log_files:
         reader = list(csv.reader(file))
         for row in reader[1:]:
             if row[2] in data.keys():
-                data[row[2]]+=0 if row[-2]=='' else float(row[-2])
+                data[row[2]]+=0 if row[-2]=='' else float(row[-2])-month[path.split()[-2][:3]]
             else:
-                data[row[2]] = 0 if row[-2]=='' else float(row[-2])
+                data[row[2]] = 0 if row[-2]=='' else float(row[-2])-month[path.split()[-2][:3]]
 
 for key,value in data.items():
     with open('holidays.csv', 'a') as f:
